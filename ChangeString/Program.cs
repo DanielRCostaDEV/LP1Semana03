61203adab119f107b807d36e35d39aa03a8b2da8
Daniel Costa <danielrcosta.dev@gmail.com>
2024-03-07 14:45:02 +0000

    Asks user for a string

---
+++ ChangeString/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write a sentence or a word: ");
+            string str = Console.ReadLine();
+        }
+    }
+}
